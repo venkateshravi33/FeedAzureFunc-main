4744ef28d7d646e672c39268485ea996a09879e6
Venkatesh Ravi <63771054+venkateshravi33@users.noreply.github.com>
2023-01-18 14:27:41 +0530

    Add files via upload

---
+++ Otto.Feed.FeedAzureFunc.Core/Interfaces/IFeedService.cs
@@ -0,0 +1,18 @@
+﻿using Otto.Feed.FeedAzureFunc.Models.DTOs;
+using Otto.Feed.FeedAzureFunc.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otto.Feed.FeedAzureFunc.Core.Interfaces
+{
+	public interface IFeedService
+	{
+        public Task<FeedDTO> addFeedAsync(FeedDTO feed);
+
+        public Task<IEnumerable<FeedDTO>> GetFeedAsync(long user_id);
+    }
+}
+
---
+++ Otto.Feed.FeedAzureFunc.Core/Services/FeedCoreService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Otto.Feed.FeedAzureFunc.Core.Interfaces;
+using Otto.Feed.FeedAzureFunc.Models.DTOs;
+using Otto.Feed.FeedAzureFunc.Models.Models;
+using Otto.Feed.FeedAzureFunc.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otto.Feed.FeedAzureFunc.Core.Services
+{
+    public class FeedCoreService : IFeedService
+    {
+        IRepositoryWrapper _repoWrapper;
+        IMapper _mapper;
+        public FeedCoreService(IRepositoryWrapper repoWrapper, IMapper mapper)
+        {
+            _repoWrapper = repoWrapper;
+            _mapper = mapper;
+        }
+        public async Task<FeedDTO> addFeedAsync(FeedDTO feed)
+        {
+            var data = await _repoWrapper.Feed.addFeedAsync(_mapper.Map<feed>(feed));
+            return _mapper.Map<FeedDTO>(data);
+        }
+
+        public async Task<IEnumerable<FeedDTO>> GetFeedAsync(long user_id)
+        {
+            var data = await _repoWrapper.Feed.GetFeedAsync(user_id);
+            return _mapper.Map<List<FeedDTO>>(data);
+        }
+    }
+}
